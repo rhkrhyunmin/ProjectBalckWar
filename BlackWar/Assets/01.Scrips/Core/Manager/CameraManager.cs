be1257456baeb35e8948db054fce975c93df6c63
rhkrhyunmin <rhkrhyunmin@gmail.com>
2024-08-30 00:07:31 +0900

    .

---
+++ BlackWar/Assets/01.Scrips/Core/Manager/CameraManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera camera;
+    public float moveSpeed = 5f;
+    public float dragSpeed = 0.1f;
+
+    private Vector2 dragOrigin;
+
+    void Update()
+    {
+        // 모바일 드래그 처리
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                dragOrigin = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                Vector2 difference = touch.position - dragOrigin;
+                MoveCamera(-difference.x * dragSpeed);
+                dragOrigin = touch.position;
+            }
+        }
+
+        // 키보드 방향키 처리
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveCamera(-moveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveCamera(moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void MoveCamera(float amount)
+    {
+        camera.transform.Translate(new Vector3(amount, 0, 0), Space.World);
+    }
+}
